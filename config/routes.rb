6e3a452790082e056ee12ef68a2dde06caf85e2c
Alden Wahsono <aldenwahsono15@icloud.com>
2022-12-26 17:52:57 +0700

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,44 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/components', to: "pages#components"
 
+  resources :schools, only: %i[index show new create] do
+    resources :profiles
+  end
+
+  resources :profiles, only: %i[index show] do
+    resources :week_menus
+    resources :allergy_profiles
+  end
+
+  resources :week_menus do
+    resources :day_menu
+  end
+
+  resources :day_menu do
+    resources :menu_dishes
+  end
+
+  resources :menu_dishes do
+    resources :dishes
+  end
+
+  resources :allergy_profiles do
+    resources :ingredients
+  end
+
+  resources :ingredients do
+    resources :dish_ingredients
+  end
+
+  resources :dish_ingredients do
+    resources :dishes
+  end
+
+  resources :dishes
+end
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-end
