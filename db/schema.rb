061108e64c6410fcdd1afe5dbe51e824db02f285
Alden Wahsono <aldenwahsono15@icloud.com>
2022-12-28 18:45:12 +0700

    weekly_menus_by_date

--- db/schema.rb
+++ db/schema.rb
@@ -117,7 +117,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_132546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
-    t.string "string"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
