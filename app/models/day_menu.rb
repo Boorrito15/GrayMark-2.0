2a17ffb74114c8af3bc013cecb2ac60371b1a5d8
Alden Wahsono <aldenwahsono15@icloud.com>
2023-01-02 03:57:13 +0700

    added cuisines

--- app/models/day_menu.rb
+++ app/models/day_menu.rb
@@ -4,4 +4,8 @@ class DayMenu < ApplicationRecord
   has_many :dishes, through: :menu_dishes
   validates :date, presence: true
   validates :date, presence: true, uniqueness: { scope: %i[week_menu] }
+
+  def self.cuisine
+    @cuisines = ["African", "American", "British", "Cajun", "Caribbean", "Chinese", "Eastern European", "European", "French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"]
+  end
 end
