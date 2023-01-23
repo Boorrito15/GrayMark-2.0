af9a60948f50c68e720130eb583f83d8f92d1bf2
Alden Wahsono <aldenwahsono15@icloud.com>
2023-01-23 20:19:17 +0700

    Search for substitute dishes

--- app/controllers/menu_dishes_controller.rb
+++ app/controllers/menu_dishes_controller.rb
@@ -5,5 +5,10 @@ class MenuDishesController < ApplicationController
     @diet = params[:diet]
     @intolerances = params[:intolerances].join(",")
     @allergies = params[:ingredients].join(",")
+    @meal_type = params[:meal_type]
+
+    @query = params[:query]
+    Spoonacular.new(@query, @cuisine, @diet, @intolerances, @allergies, "main course")
+    @schools = School.where(sql_query, query: "%#{params[:query]}%").order('name ASC')
   end
 end
