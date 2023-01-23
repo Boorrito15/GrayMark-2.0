44f673f960935ebae9694a7e4a6e1a294338ed33
Alden Wahsono <aldenwahsono15@icloud.com>
2023-01-23 12:44:08 +0700

    pass arguments to substitute a dish

--- app/controllers/menu_dishes_controller.rb
+++ app/controllers/menu_dishes_controller.rb
@@ -1,6 +1,9 @@
 class MenuDishesController < ApplicationController
   def edit
     @menu_dish = MenuDish.find(params[:id])
-    
+    @dish = @menu_dish.dish
+    @diet = params[:diet]
+    @intolerances = params[:intolerances].join(",")
+    @allergies = params[:ingredients].join(",")
   end
 end
