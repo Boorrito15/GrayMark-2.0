2507167bb8996e5ed5585013920407e337f0086d
Alden Wahsono <aldenwahsono15@icloud.com>
2023-01-24 15:34:16 +0700

    fixed search

--- app/controllers/menu_dishes_controller.rb
+++ app/controllers/menu_dishes_controller.rb
@@ -3,11 +3,10 @@ class MenuDishesController < ApplicationController
     @menu_dish = MenuDish.find(params[:id])
     @dish = @menu_dish.dish
     @diet = params[:diet]
-    @intolerances = params[:intolerances].join(",")
-    @allergies = params[:ingredients].join(",")
+    @intolerances = params[:intolerances]
+    @allergies = params[:ingredients]
     @meal_type = @menu_dish.dish.course
     params[:query].present? ? @query = params[:query] : @query = ""
-    raise
 
     @results = Spoonacular.new(@cuisine, @diet, @intolerances, @allergies, @meal_type)
     @results_dishes = @results.search(@query)
